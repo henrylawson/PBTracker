88253379470c20afeaaa5277490cc6b0720342b5
unknown <HLawson@Whitmore.corporate.thoughtworks.com>
2012-03-19 16:19:09 +0530

    Moved files into enater folder structure for when mapping is odone

--- DomainModel/Domain/PersonalBest.cs
+++ DomainModel/Domain/PersonalBest.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace DomainModel
+namespace DomainModel.Domain
 {
     public class PersonalBest
     {
--- DomainModelTest/Domain/PersonalBestTest.cs
+++ DomainModelTest/Domain/PersonalBestTest.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using DomainModel;
+using DomainModel.Domain;
 using NUnit.Framework;
 
-namespace DomainModelTest
+namespace DomainModelTest.Domain
 {
     [TestFixture]
     public class PersonalBestTest
