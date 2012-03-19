6365109a82ec26e0efea2f44f634c561886688c0
unknown <HLawson@Whitmore.corporate.thoughtworks.com>
2012-03-19 15:20:22 +0530

    Initial project with a simple class

---
+++ DomainModel/PersonalBest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DomainModel
+{
+    public class PersonalBest
+    {
+        private string _eventName;
+
+        public string EventName
+        {
+            get { return String.IsNullOrEmpty(_eventName) ? "Unknown" : _eventName; }
+            set { _eventName = value; }
+        }
+
+        public TimeSpan Time { get; set; }
+
+        public PersonalBest()
+        {
+        }
+
+        public PersonalBest(string eventName, TimeSpan time)
+        {
+            EventName = eventName;
+            Time = time;
+        }
+
+        public new string ToString()
+        {
+            return String.Format("{0}: {1}", EventName, Time.ToString());
+        }
+    }
+}
---
+++ DomainModelTest/PersonalBestTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DomainModel;
+using NUnit.Framework;
+
+namespace DomainModelTest
+{
+    [TestFixture]
+    public class PersonalBestTest
+    {
+        [Test]
+        public void ShouldHaveDefaultToStringWithNoEventNameAndTime()
+        {
+            var personalBest = new PersonalBest();
+            Assert.AreEqual("Unknown: 00:00:00", personalBest.ToString());
+        }
+
+        [Test]
+        public void ShouldHaveEventNameAndTimeInToString()
+        {
+            var personalBest = new PersonalBest("200m Freestyle", new TimeSpan(0, 0, 1, 47, 20));
+            Assert.AreEqual("200m Freestyle: 00:01:47.0200000", personalBest.ToString());
+        }
+    }
+}
